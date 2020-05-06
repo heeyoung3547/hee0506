d5f02c640fd5854439143709c9a20a8b97783970
heeyoung3547 <64881076+heeyoung3547@users.noreply.github.com>
2020-05-06 14:08:21 +0900

    Update sample.vue

--- sample.vue
+++ sample.vue
@@ -1,75 +1,85 @@
-<!-- HTML ½ÃÀÛ !-->
+<!-- HTML ì‹œìž‘ !-->
 <div id="app">
-	<v-app id="inspire">
-	    <div>
-	    <v-row no-gutters>
-	    <v-card color="black" width="100%">
-	      <div style='text-align:center'>
-	        <span class="time">2020-03-14 12:00:00</span>
-	      </div>
-	    </v-card>
-	    <v-responsive width="100%"></v-responsive>
-	      <template v-for="item in machine">
-	        <v-col :key="item.idx">
-	          <v-card>
-	            <v-row>
-	              <v-toolbar dense flat color="#b5b5b5" height="120">
-	                <span class="f_mname">{{item.mid}}</span>
-	                  <v-spacer></v-spacer>
-	                <span class="f_p">{{item.process}}</span>
-	              </v-toolbar>
-		            </v-row>
-		            <div class="text-center">
-		              <span class="f_end" style="align:center">{{item.timer}}</span>
-		            </div>
-		      	    <v-footer :fixed="true" height="100" absolute class="white">
-		              <v-row>
-		                <v-progress-linear value="15" height="94" 
-	                  color="#357d21" background-color="#828282" rounded>
-	                  <v-flex style="maxWidth:20px"></v-flex>
-	                  <span class="vt">{{ item.count}}</span>
-	                  <v-spacer></v-spacer>
-	                  <span class="vt">
-	                    <div class="text-right">{{ item.fTime }}</div>
-	                  </span>
-	                  <v-flex style="maxWidth:20px"></v-flex>
-		                </v-progress-linear>
-		              </v-row>
-		            </v-footer>
-	          </v-card>
-	        </v-col>
-	      </template>
-	</v-app>
+  <v-app id="inspire">
+      <div>
+      <v-row no-gutters >
+      <v-card color="white" width="100%">
+        <div style='text-align:center; padding:10px 0'>
+          <span class="time">2020-03-14 12:00:00</span>
+        </div>
+      </v-card>
+      <v-responsive width="100%"></v-responsive>
+        
+        
+        <template v-for="item in machine" >
+          <v-col :key="item.idx" >
+            <v-card class="ma-3">
+  
+                <v-toolbar dense flat height="100" color="#f6f6e9" >
+                  <span class="f_mname">{{item.mid}}</span>
+                    <v-spacer></v-spacer>
+                  <span class="f_p">{{item.process}}</span>
+                </v-toolbar>
+	   
+	            <div class="text-center" background-color="#F5F7FA">
+	              <span class="f_end" style="align:center">{{item.timer}}</span>
+	            </div>
+	      	    <v-footer :fixed="true" height="80" absolute class="white">
+	              <v-row >
+	                <v-progress-linear value="15" height="60" 
+                    color="#5BE7C4" background-color="#fff" rounded class="vt-wrap">
+                    <v-flex style="maxWidth:20px" ></v-flex>
+                    <span class="vt">{{ item.count}}</span>
+               
+                    <span class="vt">
+                      <div class="text-right">{{ item.fTime }}</div>
+                    </span>
+                    <v-flex style="maxWidth:20px"></v-flex>
+	                </v-progress-linear>
+	              </v-row>
+	            </v-footer>
+            </v-card>
+          </v-col>
+        </template>
+  </v-app>
 </div>
-<!-- HTML ³¡ !-->
+<!-- HTML ë !-->
 
 <style>
-<!-- CSS ½ÃÀÛ !-->
+<!-- CSS ì‹œìž‘ !-->
 .time{
 	font-size: 3vw;
-	color: #f4fc03;
+	color: #0994c7;
 	text-align: center;
 }
 .f_mname{
-	margin: 30px;
-	font-size: 3vw;
+	margin: 15 0px;
+	font-size: 2.5vw;
 }
 .f_p {
-	font-size: 3vh;
+	font-size: 3.5vh;
 	margin: 20px;
+  vertical-align:bottom;
+  bottom:0;
 }
 .vt {
 	font-size: 4vh;
-	color: #ffffff;
+	color: #333;
+}
+.vt-wrap{
+  border: 1px solid #ddd
+  
 }
 .f_end {
-	font-size: 9vw;
+  display:inline-block;
+  padding:20px 0 90px 0;
+	font-size:  2.5vw;
 }
-<!-- CSS ³¡ !-->
+<!-- CSS ë !-->
 </style>
 
 <script>
-<!-- JS ½ÃÀÛ !-->
+<!-- JS ì‹œìž‘ !-->
 new Vue({
 	  el: '#app',
 	  vuetify: new Vuetify(),
@@ -81,9 +91,9 @@ new Vue({
 	          mid : 'Puma280',
 	          idx : 1,
 	          process : 'CS98D25H49-01-2',
-	          timer : '°¡°ø¿Ï·á',
+	          timer : 'ê°€ê³µì™„ë£Œ',
 	          count : '100/100',
-	          fTime : '0 ½Ã°£ 6 ºÐ',
+	          fTime : '0 ì‹œê°„ 6 ë¶„',
 	          
 	        },
 	        {
@@ -92,7 +102,7 @@ new Vue({
 	          process : 'O0229',
 	          timer : '04:34',
 	          count : '4/100',
-	          fTime : '84 ½Ã°£ 40 ºÐ',
+	          fTime : '84 ì‹œê°„ 40 ë¶„',
 	          
 	        },
 	        {
@@ -101,7 +111,7 @@ new Vue({
 	          process : 'D580007-02-F',
 	          timer : '01:33',
 	          count : '459/500',
-	          fTime : '2 ½Ã°£ 36 ºÐ',
+	          fTime : '2 ì‹œê°„ 36 ë¶„',
 	          
 	        },
 	        {
@@ -110,7 +120,7 @@ new Vue({
 	          process : 'WGR150D35H24-02-1',
 	          timer : '00:10',
 	          count : '70/100',
-	          fTime : '1 ½Ã°£ 31 ºÐ',
+	          fTime : '1 ì‹œê°„ 31 ë¶„',
 	          
 	        },
 	        {
@@ -119,7 +129,7 @@ new Vue({
 	          process : 'CS98D25H49-02-02',
 	          timer : '02:34',
 	          count : '12/100',
-	          fTime : '5 ½Ã°£ 0 ºÐ',
+	          fTime : '5 ì‹œê°„ 0 ë¶„',
 	          
 	        },
 	        {
@@ -128,12 +138,12 @@ new Vue({
 	          process : 'JIG-CD',
 	          timer : 'READY',
 	          count : '110/200',
-	          fTime : '23 ½Ã°£ 48 ºÐ',
+	          fTime : '23 ì‹œê°„ 48 ë¶„',
 	          
 	        },
 	      ],
 	    }
 	  },
 	})
-<!-- JS ³¡ !-->
+<!-- JS ë !-->
 </script>
